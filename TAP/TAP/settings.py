c115874ebe2331c150b6553d63a877795946e52b
bohairbaloch <128802494+bohairbaloch@users.noreply.github.com>
2023-05-06 20:12:09 +1000

    Update settings.py
    Commented #MONGO_URI and #MONGO_DATABASE to resolve conflicts between mar and Marwan

--- TAP/TAP/settings.py
+++ TAP/TAP/settings.py
@@ -79,10 +79,10 @@ ITEM_PIPELINES = {
 
 
 #Cloud DB
-MONGO_URI = "mongodb+srv://userid:password@exampledomain.com/?retryWrites=true&w=majority"
+#MONGO_URI = "mongodb+srv://userid:password@exampledomain.com/?retryWrites=true&w=majority"
 #Uncomment if using local MongoDB and add credentials as well as correct ip address, then comment the above line
 #MONGO_URI = "mongodb://userid:password@192.168.1.113:27017/scrapy"
-MONGO_DATABASE ="scrapy"
+#MONGO_DATABASE ="scrapy"
 MONGO_COLL_TACTICS ="tactics"
 MONGO_COLL_SOFTWARE ="software"
 
